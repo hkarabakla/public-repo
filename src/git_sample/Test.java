fb0dbc1d2630a2ecb604be7892a702ef74a3e615
unknown <huseyin.karabakla@gmail.com>
2015-09-05 10:15:49 +0300

    Initial commit

---
+++ src/git_sample/Test.java
@@ -0,0 +1,10 @@
+package git_sample;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Just a sample project to learn git!");
+	}
+
+}
